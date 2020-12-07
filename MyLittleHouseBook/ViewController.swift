49c1a826c87964637c08d736dbb6246c2987c9a8
Jiexin Li <ljxxx1206@gmail.com>
2020-12-08 09:18:05 +1030

    Initial Commit

---
+++ MyLittleHouseBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLittleHouseBook
+//
+//  Created by Caihy on 8/12/20.
+//  Copyright © 2020 Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
